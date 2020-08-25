760b2fe0a6ba11dae024489aa71e481ac475f86a
yo higashida <penguin4731@gmail.com>
2020-08-25 18:57:18 +0900

    chore:Vuetify.jsを追加

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  devServer: {
-    port: 8080,
-    host: "0.0.0.0",
-    disableHostCheck: true,
+  "devServer": {
+    "port": 8080,
+    "host": "0.0.0.0",
+    "disableHostCheck": true
   },
-};
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
