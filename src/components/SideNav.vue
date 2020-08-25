d4ef00342dcee213b93ddbf68a7c8d5f297e7742
yo higashida <penguin4731@gmail.com>
2020-08-25 23:46:50 +0900

    feat: SideNavを追加

---
+++ src/components/SideNav.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app mini-variant="true" expand-on-hover="true">
+      <v-list-item>
+        <v-list-item-title class="title">YAKITORI</v-list-item-title>
+        <v-btn icon>
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-list-item>
+      <v-divider />
+      <v-list nav>
+        <v-list-item v-for="menu in menus" :key="menu.title" :to="menu.url">
+          <v-list-item-icon>
+            <v-icon>{{ menu.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ menu.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <router-view />
+    </v-content>
+
+    <v-footer app>
+      MADE BY YAKITORI.rb
+      <!-- フッター -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    menus: [
+      { title: 'Home', icon: 'mdi-home', url: '/' },
+      { title: 'Study Room', icon: 'mdi-apps', url: '/studyrooms' },
+      { title: 'About', icon: 'mdi-information-variant', url: '/about' },
+      { title: 'Logout', icon: 'mdi-logout', url: '/logout' }
+    ]
+  })
+}
+</script>
---
+++ src/views/Studyrooms.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1 class="display-1">自習室を探そう</h1>
+    <div>
+      <v-card tile width="250" height="250" v-for="studyroom in studyrooms" :key="studyroom.title">
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-1">{{studyroom.title}}</v-list-item-title>
+          <v-list-item-subtitle>{{studyroom.subtitle}}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-progress-linear
+          color="teal"
+          buffer-value="0"
+          :value="studyroom.enterPeople/studyroom.maxPeople*100"
+          stream
+        ></v-progress-linear>
+        <v-card-actions>
+          <v-btn text>入室する</v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      studyrooms: [
+        { title: 'Room101', subtitle: '数学がんばろう', enterPeople: '10', maxPeople: '20' },
+        { title: 'Room101', subtitle: '数学がんばろう', enterPeople: '10', maxPeople: '20' },
+        { title: 'Room101', subtitle: '数学がんばろう', enterPeople: '10', maxPeople: '20' },
+        { title: 'Room101', subtitle: '数学がんばろう', enterPeople: '10', maxPeople: '20' },
+        { title: 'Room101', subtitle: '数学がんばろう', enterPeople: '10', maxPeople: '20' },
+      ]
+    }
+  }
+}
+</script>
