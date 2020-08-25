ef9a1b91cf9e806380412ffd6a628b337192228b
yo higashida <penguin4731@gmail.com>
2020-08-26 02:55:56 +0900

    feat: 自習室を探すページを作成

---
+++ src/components/EnterRoomDialog.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-row justify="center">
+    <!-- ボタン部分 -->
+    <!-- ロックモードであるため、鍵のアイコン付きのボタンを表示する -->
+    <v-btn rounded @click.stop="dialog = true" color="primary" class="enter-btn">
+      <v-icon size="18">mdi-lock</v-icon>入室する
+    </v-btn>
+
+    <!-- ダイアログ部分 -->
+    <!-- パスワード（合言葉）の入力を求めるダイアログ -->
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-card>
+        <v-card-title>この部屋に入るには、合言葉が必要です。</v-card-title>
+        <v-card-text>
+          この部屋に入るには、合言葉が必要です。
+          <v-icon size="15">mdi-lock</v-icon>の表示がない場所は、合言葉なしで入室できます。
+          <v-text-field label="合言葉を入力" required></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="dialog = false">キャンセル</v-btn>
+          <v-btn color="green darken-1" text @click="dialog = false">入室する</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dialog: false,
+    }
+  },
+
+  props: {
+    'roomId': {
+      type: String,
+      required: true
+    },
+    'lock': {
+      type: Boolean,
+      required: true,
+    },
+    'password': {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+.enter-btn
+  margin-top: 4px
+  margin-right: 8px
+</style>
--- src/views/Studyrooms.vue
+++ src/views/Studyrooms.vue
@@ -1,38 +1,90 @@
 <template>
   <div>
-    <h1 class="display-1">自習室を探そう</h1>
-    <div>
-      <v-card tile width="250" height="250" v-for="studyroom in studyrooms" :key="studyroom.title">
-        <v-list-item-content>
-          <v-list-item-title class="headline mb-1">{{studyroom.title}}</v-list-item-title>
-          <v-list-item-subtitle>{{studyroom.subtitle}}</v-list-item-subtitle>
-        </v-list-item-content>
-        <v-progress-linear
-          color="teal"
-          buffer-value="0"
-          :value="studyroom.enterPeople/studyroom.maxPeople*100"
-          stream
-        ></v-progress-linear>
+    <h1 class="display-1" style="margin:30px">自習室を探そう</h1>
+
+    <v-container fluid>
+      <v-card
+        tile
+        class="mx-auto studyroom-card"
+        v-for="studyroom in studyrooms"
+        :key="studyroom.title"
+      >
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="headline mb-1 center">{{studyroom.title}}</v-list-item-title>
+            <v-list-item-subtitle class="center">{{studyroom.subtitle}}</v-list-item-subtitle>
+
+            <div style="margin-top:30px;margin-bottom:5px;">
+              <p style="margin-bottom:3px;">入室人数</p>
+              <!-- 入室状況のプロセスバーの表示 -->
+              <v-progress-linear
+                color="teal"
+                :value="studyroom.enterPeople/studyroom.maxPeople*100"
+              ></v-progress-linear>
+              <p style="text-align:right">{{studyroom.enterPeople}}</p>
+            </div>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
         <v-card-actions>
-          <v-btn text>入室する</v-btn>
+          <v-layout justify-center>
+            <EnterRoomDialog
+              v-if="studyroom.lock"
+              :roomId="studyroom.roomId"
+              :lock="studyroom.lock"
+              :password="studyroom.password"
+            ></EnterRoomDialog>
+            <v-btn rounded color="primary" class="enter-btn" v-else>入室する</v-btn>
+          </v-layout>
         </v-card-actions>
       </v-card>
-    </div>
+    </v-container>
   </div>
 </template>
 
 <script>
+import EnterRoomDialog from '../components/EnterRoomDialog'
+
 export default {
+  components: {
+    EnterRoomDialog,
+  },
+
   data () {
     return {
+      // studyroomに仮のデータを入れているので、これをDBデータに入れ替えてほしい
       studyrooms: [
-        { title: 'Room101', subtitle: '数学がんばろう', enterPeople: '10', maxPeople: '20' },
-        { title: 'Room101', subtitle: '数学がんばろう', enterPeople: '10', maxPeople: '20' },
-        { title: 'Room101', subtitle: '数学がんばろう', enterPeople: '10', maxPeople: '20' },
-        { title: 'Room101', subtitle: '数学がんばろう', enterPeople: '10', maxPeople: '20' },
-        { title: 'Room101', subtitle: '数学がんばろう', enterPeople: '10', maxPeople: '20' },
-      ]
+        { roomId: '32891', title: 'Room101', subtitle: '数学がんばろう', enterPeople: '10', maxPeople: '20', lock: false, password: null },
+        { roomId: '48923', title: 'Room102', subtitle: '数学がんばろう', enterPeople: '20', maxPeople: '20', lock: false, password: null },
+        { roomId: '21314', title: 'Room103', subtitle: '数学がんばろう', enterPeople: '12', maxPeople: '20', lock: true, password: 'password' },
+        { roomId: '52432', title: 'Room104', subtitle: '数学がんばろう', enterPeople: '3', maxPeople: '20', lock: false, password: null },
+        { roomId: '45232', title: 'Room105', subtitle: '数学がんばろう', enterPeople: '5', maxPeople: '20', lock: true, password: 'password' },
+        { roomId: '64953', title: 'Room106', subtitle: '数学がんばろう', enterPeople: '5', maxPeople: '20', lock: false, password: null },
+      ],
     }
   }
 }
 </script>
+
+<style scoped lang="sass">
+.headline
+  width: 100%
+  margin-top: 20px
+
+.studyroom-card
+  width: 250px
+  height: 250px
+  border-radius: 50px!important
+  margin: 30px!important
+  float: left
+  border-radius: 50px!important
+
+.center
+  text-align: center
+
+.enter-btn
+  margin-top: 4px
+  margin-right: 8px
+</style>
