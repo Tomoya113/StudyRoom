20a8b7b2c823d7d2b62f4baead74aa1b5e3d78ae
yo higashida <penguin4731@gmail.com>
2020-08-27 23:24:41 +0900

    feat: 自習室用のSideNavを追加

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <v-app>
-    <SideNav />
+    <RoomSideNav v-if="this.$route.name == 'Studyroom'" />
+    <SideNav v-else />
   </v-app>
 </template>
 
 <script>
 import SideNav from './components/SideNav'
+import RoomSideNav from './components/RoomSideNav'
 
 export default {
   name: 'App',
 
   components: {
     SideNav,
+    RoomSideNav
   },
 
   data: () => ({
