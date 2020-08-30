a9b948b0fe706bd1e9e869e000a76beaa1ac0140
Mikami <cordion_nap@yahoo.co.jp>
2020-08-30 16:42:17 +0900

    feat: authUserのuidと一致するUserの取得

--- src/store/user.js
+++ src/store/user.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 export default {
   state: {
     current_user: null,
@@ -5,12 +7,24 @@ export default {
   mutations: {
     initCurrentUser(state, user) {
       state.current_user = user;
-      console.log(user);
+      console.log(user.userId);
+      console.log(user.name);
     },
   },
   actions: {
     initCurrentUser({ commit }, user) {
-      commit('initCurrentUser', user);
+      var userRef = firebase.firestore().collection('users');
+      userRef.where('userId', '==', user.uid).get()
+        .then( (query) =>  {
+          if (query.docs.length == 1 ) {
+            query.forEach((doc) => {
+              commit('initCurrentUser', doc.data());
+            });
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     },
   },
   getters: {
