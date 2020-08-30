adb0e4c96f2a4f49c13f42ac1eb3d80e098d2ad3
Mikami Hiroki <50792596+mona-apk@users.noreply.github.com>
2020-08-30 23:22:33 +0900

    feat: routerにAboutページを追加

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Studyrooms from '../views/Studyrooms.vue'
 import Studyroom from '../views/Studyroom.vue'
+import About from '../views/About.vue'
 import User from '../views/User.vue'
 
 Vue.use(VueRouter)
@@ -27,6 +28,11 @@ const routes = [
     path: '/user',
     name: 'User',
     component: User,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
   },
 ]
 
